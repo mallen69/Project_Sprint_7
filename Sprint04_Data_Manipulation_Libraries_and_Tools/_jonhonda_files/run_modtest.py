415e5056082c61698a614059b991e3a43a56e3fc
jonhonda <33341673+hondajyh@users.noreply.github.com>
2018-01-21 08:08:32 -1000

    SQLAlchemy modules: toy example
    a basic toy example demonstrating how to divide the declarative_base, table class, and main code into 3 separate modules.

---
+++ Sprint04_Data_Manipulation_Libraries_and_Tools/_jonhonda_files/run_modtest.py
@@ -0,0 +1,4 @@
+import test_modules_1 as tm1
+
+tm1.remFile('junk.txt')
+os.remove('junk2.txt')
---
+++ Sprint04_Data_Manipulation_Libraries_and_Tools/_jonhonda_files/test_modules_1.py
@@ -0,0 +1,8 @@
+import os
+
+def remFile(filepath):
+    try:
+        os.remove(filepath)
+        print ('file removed')
+    except FileNotFoundError as err:
+        print ("no need to remove file")####it's okay if file doesn't exist. ####
